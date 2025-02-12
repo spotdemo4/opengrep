1f6e311adc77210faaa326ab73e09a470094f1f5
Dimitris Mostrous <dimitris.mostrous+aikido@gmail.com>
2025-02-12 17:32:03 +0200

    create subprocess and wait on windows (ocaml)
    This is to try and be as close as possible to execvp behaviour on Unix
    and OSX.

--- TCB/CapUnix.ml
+++ TCB/CapUnix.ml
@@ -1,4 +1,6 @@
 let execvp _caps = Unix.execvp
+let create_process _caps = Unix.create_process
+let waitpid _caps = Unix.waitpid
 let system _caps = Unix.system
 let fork _caps = Unix.fork
 let alarm _caps = Unix.alarm
