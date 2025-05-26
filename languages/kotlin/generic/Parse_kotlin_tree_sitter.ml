a10e05ed7721795b28b5aea0b4da7b4d32cf688c
maciejpirog <maciej.adam.pirog@gmail.com>
2025-05-26 23:09:59 +0200

    Fix ranges of parenthesized expressions in Kotlin

--- languages/kotlin/generic/Parse_kotlin_tree_sitter.ml
+++ languages/kotlin/generic/Parse_kotlin_tree_sitter.ml
@@ -1628,12 +1628,19 @@ and parameter_with_optional_type (env : env)
   in
   G.param_of_id v2 ~pattrs:v1 ?ptype:v3
 
+
+and adjust_range_of_parenthesized_expr (env : env)
+    (l : Tree_sitter_run.Token.t) (r : Tree_sitter_run.Token.t) (e : G.expr) : G.expr =
+  let lloc = Tok.unsafe_loc_of_tok (token env l) in
+  let rloc = Tok.unsafe_loc_of_tok (token env r) in
+  e.e_range <- Some (lloc, rloc);
+  e
+
 and parenthesized_expression (env : env)
     ((v1, v2, v3) : CST.parenthesized_expression) : G.expr =
-  let _v1 = token env v1 (* "(" *) in
-  let v2 = expression env v2 in
-  let _v3 = token env v3 (* ")" *) in
-  v2
+  expression env v2
+  |> adjust_range_of_parenthesized_expr env v1 v3
+
 
 and parenthesized_type (env : env) ((v1, v2, v3) : CST.parenthesized_type) =
   let _v1 = token env v1 (* "(" *) in
