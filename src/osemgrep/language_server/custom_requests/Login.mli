8912fbfecef040723a51a4fa668eed66d78101c7
Dimitris Mostrous <dimitris.mostrous+aikido@gmail.com>
2025-07-05 19:27:59 +0100

    remove `login` and `publish` from cli dune file

--- src/osemgrep/language_server/custom_requests/Login.mli
+++
@@ -1,7 +0,0 @@
-val meth : string
-(** method to match on: semgrep/login *)
-
-val on_request :
-  Session.t -> Jsonrpc.Structured.t option -> Yojson.Safe.t option
-(** Called by client to login to Semgrep App, returning the [id] of the
-      login session and the [uri] to log in to. Returns None if already logged in **)
--- src/osemgrep/language_server/custom_requests/LoginStatus.ml
+++
@@ -1,4 +0,0 @@
-let meth = "semgrep/loginStatus"
-
-let on_request (_session : Session.t) _params : Yojson.Safe.t option =
-  Some (`Assoc [ ("loggedIn", `Bool (Semgrep_login.is_logged_in_weak ())) ])
